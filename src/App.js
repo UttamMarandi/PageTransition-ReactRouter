9b1b916bfd42d948349871efe1675a8ad057e6ed
Uttam Marandi <uttammarandi007@gmail.com>
2021-09-09 23:56:09 +0530

    understanding how works , home page animation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+
 //Pages
 import Home from "./pages/home";
 import Model from "./pages/model";
@@ -23,7 +24,7 @@ function App() {
         render={({ location }) => (
           // initial flase allows us to have no animation when a page is refreshed or user is coming from external source. Need to come from internal source
           // exitBeforeEnter: If set to true, AnimatePresence will only render one component at a time. The exiting component will finished its exit animation before the entering component is rendered.
-          <AnimatePresence initial={false} exitBeforeEnter>
+          <AnimatePresence exitBeforeEnter>
             <Switch location={location} key={location.pathname}>
               <Route
                 exact
